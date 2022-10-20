6ef9f8e5c6da0d6250b04b37d7683d4937e49030
Druzhininad <darya.druzhinina.2003@mail.ru>
2022-10-20 21:17:42 +0300

    аа

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
 import logging
 
-from telegram import Update, KeyboardButton
-from telegram import ReplyKeyboardMarkup
-from telegram.constants import ParseMode
-from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
+from googletrans import Translator
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -11,25 +10,43 @@ logging.basicConfig(
 )
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f"Эй, {update.message.chat.first_name}! Где ваша вежливость?"
-                                    f"\nНапишите <i>здравствуйте</i>, чтобы запустить меня", parse_mode=ParseMode.HTML)
+async def start(update, _):
+    await update.message.reply_text(f"Здравствуйте! Напишите язык, на который нужно перевести.\n"
+                                    f"Доступные языки:\n"
+                                    f"en - английский\n"
+                                    f"fr - французский\n"
+                                    f"de - немецкий\n"
+                                    f"it - итальянский\n"
+                                    f"ru - русский\n"
+                                    f"es - испанский\n"
+                                    f"tr - турецкий\n"
+                                    f"Формат: /rem en")
 
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text
-    if "здравствуйте" in text.lower():
-        await update.message.reply_text(f"И вам не хворать! Что сегодня хотите перевести?")
+async def rem(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if len(context.args) != 1:
+        await update.message.reply_text('Укажите, пожалуйста, язык единственным параметром')
     else:
-        await update.message.reply_text(
-            "Ничего не понял... Может попробуете еще раз?")
+        value = context.args[0]
+        context.user_data['language'] = value
+        await update.message.reply_text(f'Отлично! Выбранный язык: {value}\n'
+                                        f'Теперь напишите фразу\n'
+                                        f'Пример: /translate text')
+
+
+async def trans(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    translator = Translator()
+    res = translator.translate(f'{context.args}', dest=context.user_data['language'])
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Перевод: {res.text}')
 
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('5514647661:AAGdRD2u3KH0Slf6ugtPdzNm5TbIiGAZnr4').build()
-    hello_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), hello)
+    application = ApplicationBuilder().token('5514647661:AAHKRnXMae36RLPO80G5zylR4QIjMod3MRI').build()
     start_handler = CommandHandler('start', start)
+    trans_handler = CommandHandler('translate', trans)
+    rem_handler = CommandHandler('rem', rem)
     application.add_handler(start_handler)
-    application.add_handler(hello_handler)
+    application.add_handler(trans_handler)
+    application.add_handler(rem_handler)
 
     application.run_polling()
